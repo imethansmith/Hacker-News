89fa6ddc221f93dda02a06baa458c24ccb34ffd3
Ethan Smith <edhsmith@icloud.com>
2021-03-16 20:46:40 +1300

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Ethan Smith on 16/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
